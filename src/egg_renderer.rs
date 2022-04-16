838acedc2eaf51544d0fb519040b8fad4e86ed69
Malacath <h.baensch.92@gmail.com>
2022-04-16 17:17:41 +0200

    Move pan flag to 3 and add plain pride flag to 6 instead

--- src/egg_renderer.rs
+++ src/egg_renderer.rs
@@ -10,6 +10,73 @@ use crate::spelunkicon::Spelunkicon;
 pub struct PrideRenderer {}
 
 impl PrideRenderer {
+    fn render_pansexual_flag(
+        &self,
+        base_image: &mut RgbaImage,
+        sheets: &Sheets,
+        config: &Spelunkicon,
+        rng: &mut StdRng,
+    ) {
+        let w = config.grid_width as u32;
+
+        // Tiles
+        {
+            let guts_tile = sheets
+                .sheet_floorstyled_from_biome(&Biome::Guts)
+                .unwrap()
+                .view(1 * TILE_WIDTH, 4 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            let y = 0 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(base_image, &guts_tile, x, y);
+            }
+        }
+
+        {
+            let gold_tile = sheets
+                .sheet_floorstyled_from_biome(&Biome::CityOfGold)
+                .unwrap()
+                .view(1 * TILE_WIDTH, 5 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            let y = 1 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(base_image, &gold_tile, x, y);
+            }
+        }
+
+        {
+            let tidepool_tile = sheets
+                .sheet_floor_from_biome(&Biome::TidePool)
+                .unwrap()
+                .view(0 * TILE_WIDTH, 0 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            let y = 2 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(base_image, &tidepool_tile, x, y);
+            }
+        }
+
+        // Deco
+        {
+            let tide_pool = &sheets.sheet_floor_from_biome(&Biome::TidePool).unwrap();
+            let up_deco = vec![
+                tide_pool.view(5 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                tide_pool.view(6 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                tide_pool.view(7 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+            ];
+            let y = 2 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(
+                    base_image,
+                    up_deco.choose(rng).unwrap(),
+                    x,
+                    y - TILE_HEIGHT / 2,
+                );
+            }
+        }
+    }
+
     fn render_nonbinary_flag(
         &self,
         base_image: &mut RgbaImage,
@@ -71,7 +138,7 @@ impl PrideRenderer {
         // No Decos
     }
 
-    fn render_pansexual_flag(
+    fn render_gay_pride_flag(
         &self,
         base_image: &mut RgbaImage,
         sheets: &Sheets,
@@ -80,59 +147,151 @@ impl PrideRenderer {
     ) {
         let w = config.grid_width as u32;
 
-        // Tiles
         {
-            let guts_sheet = sheets.sheet_floorstyled_from_biome(&Biome::Guts).unwrap();
-            let guts_tile_top =
-                guts_sheet.view(1 * TILE_WIDTH, 3 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
-            let guts_tile_bot =
-                guts_sheet.view(1 * TILE_WIDTH, 4 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            let vlad_tile = sheets
+                .sheet_floorstyled_from_biome(&Biome::Vlad)
+                .unwrap()
+                .view(1 * TILE_WIDTH, 5 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
             let y = 0 * TILE_HEIGHT;
             for i in 0..w {
                 let x = i * TILE_WIDTH;
-                overlay(base_image, &guts_tile_top, x, y);
-                overlay(base_image, &guts_tile_bot, x, y + TILE_HEIGHT);
+                overlay(base_image, &vlad_tile, x, y);
             }
         }
 
         {
-            let gold_sheet = sheets
+            let cave_tile = sheets.sheet_floor_from_biome(&Biome::Cave).unwrap().view(
+                0 * TILE_WIDTH,
+                0 * TILE_HEIGHT,
+                TILE_WIDTH,
+                TILE_HEIGHT,
+            );
+            let y = 1 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(base_image, &cave_tile, x, y);
+            }
+        }
+
+        {
+            let gold_tile = sheets
                 .sheet_floorstyled_from_biome(&Biome::CityOfGold)
-                .unwrap();
-            let gold_tile_top =
-                gold_sheet.view(1 * TILE_WIDTH, 2 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
-            let gold_tile_bot =
-                gold_sheet.view(1 * TILE_WIDTH, 4 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+                .unwrap()
+                .view(1 * TILE_WIDTH, 5 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
             let y = 2 * TILE_HEIGHT;
             for i in 0..w {
                 let x = i * TILE_WIDTH;
-                overlay(base_image, &gold_tile_top, x, y);
-                overlay(base_image, &gold_tile_bot, x, y + TILE_HEIGHT);
+                overlay(base_image, &gold_tile, x, y);
             }
         }
 
         {
-            let tidepool_tile = sheets
-                .sheet_floor_from_biome(&Biome::TidePool)
+            let jungle_tile = sheets.sheet_floor_from_biome(&Biome::Jungle).unwrap().view(
+                0 * TILE_WIDTH,
+                0 * TILE_HEIGHT,
+                TILE_WIDTH,
+                TILE_HEIGHT,
+            );
+            let y = 3 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(base_image, &jungle_tile, x, y);
+            }
+        }
+
+        {
+            let pagoda_tile = sheets
+                .sheet_floorstyled_from_biome(&Biome::TidePool)
                 .unwrap()
-                .view(0 * TILE_WIDTH, 0 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+                .view(1 * TILE_WIDTH, 5 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
             let y = 4 * TILE_HEIGHT;
             for i in 0..w {
                 let x = i * TILE_WIDTH;
-                overlay(base_image, &tidepool_tile, x, y);
-                overlay(base_image, &tidepool_tile, x, y + TILE_HEIGHT);
+                overlay(base_image, &pagoda_tile, x, y);
+            }
+        }
+
+        {
+            let eggplant_tile = sheets
+                .sheet_floor_from_biome(&Biome::Eggplant)
+                .unwrap()
+                .view(0 * TILE_WIDTH, 0 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            let y = 5 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(base_image, &eggplant_tile, x, y);
             }
         }
 
         // Deco
         {
-            let tide_pool = &sheets.sheet_floor_from_biome(&Biome::TidePool).unwrap();
+            let cave = &sheets.sheet_floor_from_biome(&Biome::Cave).unwrap();
             let up_deco = vec![
-                tide_pool.view(5 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
-                tide_pool.view(6 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
-                tide_pool.view(7 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                cave.view(5 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                cave.view(6 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                cave.view(7 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
             ];
-            let y = 4 * TILE_HEIGHT;
+            let down_deco = vec![
+                cave.view(5 * TILE_WIDTH, 7 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                cave.view(6 * TILE_WIDTH, 7 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                cave.view(7 * TILE_WIDTH, 7 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+            ];
+            let y = 1 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(
+                    base_image,
+                    up_deco.choose(rng).unwrap(),
+                    x,
+                    y - TILE_HEIGHT / 2,
+                );
+                overlay(
+                    base_image,
+                    down_deco.choose(rng).unwrap(),
+                    x,
+                    y + TILE_HEIGHT / 2,
+                );
+            }
+        }
+
+        {
+            let jungle = &sheets.sheet_floor_from_biome(&Biome::Jungle).unwrap();
+            let up_deco = vec![
+                jungle.view(5 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                jungle.view(6 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                jungle.view(7 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+            ];
+            let down_deco = vec![
+                jungle.view(5 * TILE_WIDTH, 7 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                jungle.view(6 * TILE_WIDTH, 7 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                jungle.view(7 * TILE_WIDTH, 7 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+            ];
+            let y = 3 * TILE_HEIGHT;
+            for i in 0..w {
+                let x = i * TILE_WIDTH;
+                overlay(
+                    base_image,
+                    up_deco.choose(rng).unwrap(),
+                    x,
+                    y - TILE_HEIGHT / 2,
+                );
+                overlay(
+                    base_image,
+                    down_deco.choose(rng).unwrap(),
+                    x,
+                    y + TILE_HEIGHT / 2,
+                );
+            }
+        }
+
+        {
+            let eggplant = &sheets.sheet_floor_from_biome(&Biome::Eggplant).unwrap();
+            let up_deco = vec![
+                eggplant.view(5 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                eggplant.view(6 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+                eggplant.view(7 * TILE_WIDTH, 6 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT),
+            ];
+            let y = 5 * TILE_HEIGHT;
             for i in 0..w {
                 let x = i * TILE_WIDTH;
                 overlay(
@@ -336,6 +495,16 @@ impl PrideRenderer {
         }
     }
 
+    fn render_3_pride(
+        &self,
+        base_image: &mut RgbaImage,
+        sheets: &Sheets,
+        config: &Spelunkicon,
+        rng: &mut StdRng,
+    ) {
+        self.render_pansexual_flag(base_image, sheets, config, rng);
+    }
+
     fn render_4_pride(
         &self,
         base_image: &mut RgbaImage,
@@ -353,7 +522,7 @@ impl PrideRenderer {
         config: &Spelunkicon,
         rng: &mut StdRng,
     ) {
-        self.render_pansexual_flag(base_image, sheets, config, rng);
+        self.render_gay_pride_flag(base_image, sheets, config, rng);
     }
 
     fn render_8_pride(
@@ -374,6 +543,7 @@ impl PrideRenderer {
         rng: &mut StdRng,
     ) {
         match config.grid_height {
+            3 => self.render_3_pride(base_image, sheets, config, rng),
             4 => self.render_4_pride(base_image, sheets, config, rng),
             6 => self.render_6_pride(base_image, sheets, config, rng),
             8 => self.render_8_pride(base_image, sheets, config, rng),
